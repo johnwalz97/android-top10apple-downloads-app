0f854fc045f32f151b78a4f040d4e465f0469cd6
johnwalz97 <johnwalz97@gmail.com>
2016-01-20 17:58:36 -0800

    initial commmit

---
+++ app/src/main/java/com/johnwwalz/top10downloader/Application.java
@@ -0,0 +1,39 @@
+package com.johnwwalz.top10downloader;
+
+/**
+ * Created by john on 1/20/2016.
+ */
+public class Application {
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" + "Artist: " + getArtist() + "\n" + "Release Date: " + getReleaseDate() + "\n";
+    }
+}
